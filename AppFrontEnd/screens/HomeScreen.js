892da1bc244e356a4941e698efd7272bdacdf6c9
matthew <matthew.chiang.myc@gmail.com>
2019-02-16 16:46:16 -0500

    new recipe route

--- AppFrontEnd/screens/HomeScreen.js
+++ AppFrontEnd/screens/HomeScreen.js
@@ -5,12 +5,15 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TouchableHighlight,
   TouchableOpacity,
   View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
+
+
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -24,15 +27,15 @@ export default class HomeScreen extends React.Component {
 
           <View style={styles.container}>
             <Text style={styles.getStartedText}>
-              Smart Stove
+              Smart Stoves
             </Text>
           </View>
 
-          <View style={styles.container}>
+          <TouchableHighlight style={styles.button} onPress={() => this.props.navigation.navigate('CreateStack')}>
             <Text style={styles.getStartedText}>
-              New Recipe
+              Create New Recipe
             </Text>
-          </View>
+          </TouchableHighlight>
 
           <View style={styles.container}>
             <Text style={styles.getStartedText}>
@@ -42,79 +45,7 @@ export default class HomeScreen extends React.Component {
 
         </ScrollView>
       </View>
-      // <View style={styles.container}>
-      //   <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-      //     <View style={styles.welcomeContainer}>
-      //       <Image
-      //         source={
-      //           __DEV__
-      //             ? require('../assets/images/robot-dev.png')
-      //             : require('../assets/images/robot-prod.png')
-      //         }
-      //         style={styles.welcomeImage}
-      //       />
-      //     </View>
-      //
-          // <View style={styles.container}>
-          //   <Text style={styles.getStartedText}>
-          //     Smart Stove
-          //   </Text>
-          // </View>
-      //
-      //     <View style={styles.getStartedContainer}>
-      //       {this._maybeRenderDevelopmentModeWarning()}
-      //
-      //       <Text style={styles.getStartedText}>Get started by opening</Text>
-      //
-      //       <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-      //         <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-      //       </View>
-      //
-      //       <Text style={styles.getStartedText}>
-      //         Change this text and your app will automatically reload.
-      //       </Text>
-      //     </View>
-      //
-      //     <View style={styles.helpContainer}>
-      //       <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-      //         <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-      //       </TouchableOpacity>
-      //     </View>
-      //
-      //   </ScrollView>
-      //
-      //   <View style={styles.tabBarInfoContainer}>
-      //     <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-      //
-      //     <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-      //       <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-      //     </View>
-      //   </View>
-      // </View>
     );
-  }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
   }
 
   _handleLearnMorePress = () => {
@@ -176,6 +107,10 @@ const styles = StyleSheet.create({
     lineHeight: 24,
     textAlign: 'center',
     margin: 30
+  },
+  button:{
+    flex: 1,
+    backgroundColor: '#DDD'
   },
   tabBarInfoContainer: {
     position: 'absolute',
